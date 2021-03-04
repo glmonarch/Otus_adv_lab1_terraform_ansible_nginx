39585246ba72c4dca6d49089c4c0152a366c8357
glmonarch <glmonarch@users.noreply.github.com>
2021-03-04 15:50:15 +0300

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -3,7 +3,7 @@ resource "ah_cloud_server" "srv-1" {
   datacenter = var.ah_datacenter
   image = var.ah_image_type
   product = var.ah_server_type
-  ssh_keys = ["02:84:8c:29:4d:16:10:12:3e:7e:48:fc:d4:46:d8:81"]
+  ssh_keys = ["ssh_public_key_fingerprint_from_AdvancedHosting_panel"]
   create_public_ip_address = "true"
 
   /* If you will get error with "local-exec" please uncomment the block "remote-exec" below to make sure that the "local-exec" will reach our newly created VM
